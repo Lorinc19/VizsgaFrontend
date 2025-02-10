dabbbd450ea56b36b89bbc0fcab08620d6523cbc
KrizsanMark <krizsanm@kkszki.hu>
2025-02-10 13:32:53 +0100

    Mockaroo adatok sqlbe, tezstelésre

---
+++ hirdetes.sql
@@ -0,0 +1,5 @@
+insert into hirdetes (id, FelhasznaloId, Leiras, Elerhetoseg, HirdetesNev) values (1, 1, 'Miskolc', '+36-70-934-3071', 'Lőrinc');
+insert into hirdetes (id, FelhasznaloId, Leiras, Elerhetoseg, HirdetesNev) values (2, 2, 'Budapest', '+36-30-517-2022', 'Márk');
+insert into hirdetes (id, FelhasznaloId, Leiras, Elerhetoseg, HirdetesNev) values (3, 3, 'Debrecen', '+36-20-654-7021', 'Enikő');
+insert into hirdetes (id, FelhasznaloId, Leiras, Elerhetoseg, HirdetesNev) values (4, 4, 'Kecskemét', '+36-70-554-2435', 'Gábor');
+insert into hirdetes (id, FelhasznaloId, Leiras, Elerhetoseg, HirdetesNev) values (5, 5, 'Parasznya', '+36-30-466-2238', 'Andrea');
