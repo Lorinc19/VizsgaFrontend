cae5e74065a01c975e7fdeb7e51f1d1f52394d12
KrizsanMark <91114588+KrizsanMark@users.noreply.github.com>
2025-03-24 10:29:56 +0100

    HirdetesAdatok/Hirdetes táblák össze lettek vonva

--- szakmaivizsga.sql
+++ szakmaivizsga.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Már 19. 09:06
--- Kiszolgáló verziója: 10.4.20-MariaDB
--- PHP verzió: 7.3.29
+-- Létrehozás ideje: 2025. Már 23. 22:25
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `szakmaivizsga`
 --
-CREATE DATABASE IF NOT EXISTS `szakmaivizsga` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+CREATE DATABASE IF NOT EXISTS `szakmaivizsga` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `szakmaivizsga`;
 
 -- --------------------------------------------------------
@@ -31,10 +31,10 @@ USE `szakmaivizsga`;
 
 CREATE TABLE `aspnetroleclaims` (
   `Id` int(11) NOT NULL,
-  `RoleId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `ClaimType` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `ClaimValue` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `RoleId` varchar(255) NOT NULL,
+  `ClaimType` longtext DEFAULT NULL,
+  `ClaimValue` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -43,11 +43,18 @@ CREATE TABLE `aspnetroleclaims` (
 --
 
 CREATE TABLE `aspnetroles` (
-  `Id` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Name` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `NormalizedName` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `ConcurrencyStamp` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `Id` varchar(255) NOT NULL,
+  `Name` varchar(256) DEFAULT NULL,
+  `NormalizedName` varchar(256) DEFAULT NULL,
+  `ConcurrencyStamp` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `aspnetroles`
+--
+
+INSERT INTO `aspnetroles` (`Id`, `Name`, `NormalizedName`, `ConcurrencyStamp`) VALUES
+('0797dc41-e78a-48e4-a65d-1d88b325fcdf', 'Admin', 'ADMIN', NULL);
 
 -- --------------------------------------------------------
 
@@ -57,10 +64,10 @@ CREATE TABLE `aspnetroles` (
 
 CREATE TABLE `aspnetuserclaims` (
   `Id` int(11) NOT NULL,
-  `UserId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `ClaimType` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `ClaimValue` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `UserId` varchar(255) NOT NULL,
+  `ClaimType` longtext DEFAULT NULL,
+  `ClaimValue` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,11 +76,11 @@ CREATE TABLE `aspnetuserclaims` (
 --
 
 CREATE TABLE `aspnetuserlogins` (
-  `LoginProvider` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `ProviderKey` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `ProviderDisplayName` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `UserId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `LoginProvider` varchar(255) NOT NULL,
+  `ProviderKey` varchar(255) NOT NULL,
+  `ProviderDisplayName` longtext DEFAULT NULL,
+  `UserId` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -82,9 +89,16 @@ CREATE TABLE `aspnetuserlogins` (
 --
 
 CREATE TABLE `aspnetuserroles` (
-  `UserId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `RoleId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `UserId` varchar(255) NOT NULL,
+  `RoleId` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `aspnetuserroles`
+--
+
+INSERT INTO `aspnetuserroles` (`UserId`, `RoleId`) VALUES
+('2df77e72-c1c5-4807-bdc0-7c238477ba0b', '0797dc41-e78a-48e4-a65d-1d88b325fcdf');
 
 -- --------------------------------------------------------
 
@@ -93,25 +107,34 @@ CREATE TABLE `aspnetuserroles` (
 --
 
 CREATE TABLE `aspnetusers` (
-  `Id` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Keresztnev` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `Vezeteknev` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
+  `Id` varchar(255) NOT NULL,
+  `Keresztnev` longtext DEFAULT NULL,
+  `Vezeteknev` longtext DEFAULT NULL,
   `Kor` int(11) DEFAULT NULL,
-  `UserName` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `NormalizedUserName` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `Email` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `NormalizedEmail` varchar(256) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
+  `UserName` varchar(256) DEFAULT NULL,
+  `NormalizedUserName` varchar(256) DEFAULT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `NormalizedEmail` varchar(256) DEFAULT NULL,
   `EmailConfirmed` tinyint(1) NOT NULL,
-  `PasswordHash` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `SecurityStamp` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `ConcurrencyStamp` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `PhoneNumber` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
+  `PasswordHash` longtext DEFAULT NULL,
+  `SecurityStamp` longtext DEFAULT NULL,
+  `ConcurrencyStamp` longtext DEFAULT NULL,
+  `PhoneNumber` longtext DEFAULT NULL,
   `PhoneNumberConfirmed` tinyint(1) NOT NULL,
   `TwoFactorEnabled` tinyint(1) NOT NULL,
   `LockoutEnd` datetime DEFAULT NULL,
   `LockoutEnabled` tinyint(1) NOT NULL,
   `AccessFailedCount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `aspnetusers`
+--
+
+INSERT INTO `aspnetusers` (`Id`, `Keresztnev`, `Vezeteknev`, `Kor`, `UserName`, `NormalizedUserName`, `Email`, `NormalizedEmail`, `EmailConfirmed`, `PasswordHash`, `SecurityStamp`, `ConcurrencyStamp`, `PhoneNumber`, `PhoneNumberConfirmed`, `TwoFactorEnabled`, `LockoutEnd`, `LockoutEnabled`, `AccessFailedCount`) VALUES
+('261cc2e8-b817-4c57-9d6f-0d994c2a9b2f', 'Valamkinrj', 'sdfdg', 55, 'VAlaki', 'VALAKI', 'valaki@gmail.com', 'VALAKI@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEKMLydxbyDHB47dqWviIqp93GVJOaoKdcx0xvNCywHOHE+lpmHM06NM/J+u2o9lP7g==', 'CNDDMHWIPXI34RR5KJX4TL2XDKNK346Q', 'aa794383-1247-4088-8e4a-0144220099db', NULL, 0, 0, NULL, 1, 0),
+('2df77e72-c1c5-4807-bdc0-7c238477ba0b', 'Almafa', 'Ága', 20, 'Alma', 'ALMA', 'alma@gmail.com', 'ALMA@GMAIL.COM', 0, 'AQAAAAIAAYagAAAAEDZt6iGOUMB5yn8N2BmQ/yOFqJlDbSiuIl3RSbLG7wdJOQ28d2Mlc126vrB/S7Z6eA==', 'M5KNT5BP37UVGUZ2I2S6EN7ZNJQ5XYSY', '67f2bd75-72b0-475c-aa75-e50d2ab08c0c', NULL, 0, 0, NULL, 1, 0),
+('d4fdf283-04f0-414b-9ca7-9ab9166021d9', 'Józsi', 'Akna', 24, 'Senky', 'SENKY', 'seni@gmnail.com', 'SENI@GMNAIL.COM', 0, 'AQAAAAIAAYagAAAAEMwKwbwtMCp2MIpwfARHmDCVhBU2c1iKH46OoqCrUS4ND4TWo+rXew0AmMPL97di0A==', 'IIE6VP7IR5W7YE5Q2UV6E2EH66ESFGHR', '4e554660-0ae2-4ab2-9a0e-2884ff913999', NULL, 0, 0, NULL, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -120,11 +143,11 @@ CREATE TABLE `aspnetusers` (
 --
 
 CREATE TABLE `aspnetusertokens` (
-  `UserId` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `LoginProvider` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Name` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Value` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `UserId` varchar(255) NOT NULL,
+  `LoginProvider` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Value` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -133,32 +156,29 @@ CREATE TABLE `aspnetusertokens` (
 --
 
 CREATE TABLE `hirdetes` (
-  `ID` int(11) NOT NULL,
-  `FelhasznaloID` varchar(255) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Leiras` text COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Elerhetoseg` varchar(100) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Hirdetesnev` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
-  `KepURL` longtext COLLATE utf8mb4_hungarian_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
--- --------------------------------------------------------
+  `ID` char(36) NOT NULL,
+  `FelhasznaloID` varchar(255) NOT NULL,
+  `Hirdetesnev` varchar(50) NOT NULL,
+  `Leiras` varchar(500) NOT NULL,
+  `Ar` int(100) NOT NULL,
+  `Elerhetoseg` varchar(50) NOT NULL,
+  `Orszag` varchar(50) NOT NULL,
+  `Varmegye` varchar(50) NOT NULL,
+  `Telepules` varchar(50) NOT NULL,
+  `Utcahazszam` varchar(100) NOT NULL,
+  `Tipus` varchar(50) NOT NULL,
+  `Kiadasiidotartam` varchar(50) NOT NULL,
+  `Gyerekbarat` tinyint(1) NOT NULL,
+  `Allatbarat` tinyint(1) NOT NULL,
+  `KepURL` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Tábla szerkezet ehhez a táblához `hirdetesadatok`
+-- A tábla adatainak kiíratása `hirdetes`
 --
 
-CREATE TABLE `hirdetesadatok` (
-  `HirdetesID` int(11) NOT NULL,
-  `Orszag` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `Varmegye` varchar(50) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Telepules` varchar(50) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Utcahazszam` varchar(100) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Tipus` varchar(50) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL',
-  `Ar` decimal(10,2) DEFAULT NULL,
-  `Gyerekbarat` tinyint(1) DEFAULT 0,
-  `Allatbarat` tinyint(1) DEFAULT 0,
-  `Kiadasiidotartam` varchar(50) COLLATE utf8mb4_hungarian_ci DEFAULT 'NULL'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+INSERT INTO `hirdetes` (`ID`, `FelhasznaloID`, `Hirdetesnev`, `Leiras`, `Ar`, `Elerhetoseg`, `Orszag`, `Varmegye`, `Telepules`, `Utcahazszam`, `Tipus`, `Kiadasiidotartam`, `Gyerekbarat`, `Allatbarat`, `KepURL`) VALUES
+('3f383177-2b45-4028-b224-2b284bda875d', '2df77e72-c1c5-4807-bdc0-7c238477ba0b', 'Alma', 'Alma', 25544, 'Alma', 'Alma', 'Alma', 'Alma', 'Alma', 'stAlmaring', 'Alma', 1, 1, 'Alma');
 
 -- --------------------------------------------------------
 
@@ -167,9 +187,9 @@ CREATE TABLE `hirdetesadatok` (
 --
 
 CREATE TABLE `__efmigrationshistory` (
-  `MigrationId` varchar(150) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `ProductVersion` varchar(32) COLLATE utf8mb4_hungarian_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexek a kiírt táblákhoz
@@ -229,13 +249,7 @@ ALTER TABLE `aspnetusertokens`
 --
 ALTER TABLE `hirdetes`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `hirdetes_ibfk_1` (`FelhasznaloID`);
-
---
--- A tábla indexei `hirdetesadatok`
---
-ALTER TABLE `hirdetesadatok`
-  ADD PRIMARY KEY (`HirdetesID`);
+  ADD KEY `FelhasznaloID` (`FelhasznaloID`);
 
 --
 -- A tábla indexei `__efmigrationshistory`
@@ -258,12 +272,6 @@ ALTER TABLE `aspnetroleclaims`
 --
 ALTER TABLE `aspnetuserclaims`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT a táblához `hirdetes`
---
-ALTER TABLE `hirdetes`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Megkötések a kiírt táblákhoz
@@ -304,13 +312,7 @@ ALTER TABLE `aspnetusertokens`
 -- Megkötések a táblához `hirdetes`
 --
 ALTER TABLE `hirdetes`
-  ADD CONSTRAINT `hirdetes_ibfk_1` FOREIGN KEY (`FelhasznaloID`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE;
-
---
--- Megkötések a táblához `hirdetesadatok`
---
-ALTER TABLE `hirdetesadatok`
-  ADD CONSTRAINT `hirdetesadatok_ibfk_1` FOREIGN KEY (`HirdetesID`) REFERENCES `hirdetes` (`ID`) ON DELETE CASCADE;
+  ADD CONSTRAINT `hirdetes_ibfk_1` FOREIGN KEY (`FelhasznaloID`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
